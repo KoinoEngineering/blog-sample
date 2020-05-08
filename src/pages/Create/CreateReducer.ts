2e62d1d45c479434c2c3dc8441341f3021f564d9
KoinoEngineering <40349284+KoinoEngineering@users.noreply.github.com>
2020-05-08 10:07:03 +0900

    #7 Revert "Merge pull request #6 from KoinoEngineering/#4/apply_pure_component"
    This reverts commit 48a4138d8a8bb979af0344336486920b44ffce58.

--- src/pages/Create/CreateReducer.ts
+++ src/pages/Create/CreateReducer.ts
@@ -1,14 +1,31 @@
 import { Reducer } from "redux";
-import { CreateActions } from "./CreateAction";
+import { ActionType, CreateActions } from "./CreateAction";
 
 export interface CreateState {
+    submitting: boolean;
 }
 
 const initialState = (): CreateState => ({
+    submitting: false,
 });
 
 const create: Reducer<CreateState, CreateActions> = (state = initialState(), action) => {
     switch (action.type) {
+        case ActionType.CHANGE_VALUES:
+            return {
+                ...state,
+                ...action.payload
+            };
+        case ActionType.SUBMIT:
+            return {
+                ...state,
+                submitting: true
+            };
+        case ActionType.SUBMIT_END:
+            return {
+                ...state,
+                submitting: false
+            };
         default:
             return state;
     }
